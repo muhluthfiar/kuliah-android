562eff8fefd3ee17d683a81420f6c0a0517bd3ed
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2022-11-29 14:06:34 +0700

    updated firebaseapp

--- firebaseapp/app/src/main/java/com/example/firebaseapp/AdapterMahasiswa.java
+++ firebaseapp/app/src/main/java/com/example/firebaseapp/AdapterMahasiswa.java
@@ -1,15 +1,23 @@
 package com.example.firebaseapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -39,6 +47,46 @@ public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.View
         final ModelMahasiswa data = listMahasiswa.get(position);
         holder.tv_nama.setText("Nama : " + data.getNama());
         holder.tv_matkul.setText("Mata Kuliah : " + data.getMatkul());
+
+        holder.card_hasil.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                FragmentManager manager = ((AppCompatActivity)activity).getSupportFragmentManager();
+                DialogForm dialog = new DialogForm(data.getNama(), data.getMatkul(), data.getKey(), "Ubah");
+                dialog.show(manager, "Form Edit");
+                return true;
+            }
+        });
+
+        holder.btn_hapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        database.child("Mahasiswa").child(data.getKey()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(activity, "Data Berhasil Dihapus!", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(activity, "Gagal Menghapus Data!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }).setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                }).setMessage("Apakah yakin untuk menghapus " + data.getNama() + "?");
+
+                builder.show();
+            }
+        });
     }
 
     @Override
@@ -49,6 +97,7 @@ public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.View
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView tv_nama, tv_matkul;
+        ImageView btn_hapus;
         CardView card_hasil;
 
         public ViewHolder(@NonNull View itemView) {
@@ -56,6 +105,7 @@ public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.View
 
             tv_nama = itemView.findViewById(R.id.tv_nama);
             tv_matkul = itemView.findViewById(R.id.tv_matkul);
+            btn_hapus = itemView.findViewById(R.id.hapus);
             card_hasil = itemView.findViewById(R.id.card_hasil);
 
         }
