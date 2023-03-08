44fd1db69a214cb5a9d504171f7324034c534375
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2023-03-08 09:44:23 +0700

    sensor

--- appWidget/app/src/main/java/com/example/appwidget/NewAppWidget.java
+++ appWidget/app/src/main/java/com/example/appwidget/NewAppWidget.java
@@ -16,32 +16,42 @@ import java.util.Date;
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+    //variable untuk key shared preference
     private static final String mSharedPrefile = "com.example.android.appwidget";
     private static final String COUNT_KEY = "count";
 
+    // fungsi ini dipanggil saat appwidget update pada waktu tertentu
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+        // mengambil nilai dari sharedpreferences
         SharedPreferences prefs = context.getSharedPreferences(mSharedPrefile, 0);
         int count = prefs.getInt(COUNT_KEY+appWidgetId, 0);
         count++;
-
+        // mengambil date dalam bentuk string saat method dipanggil
         String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
 
+        // membuat sebuah remoteviews objek yang akan menampilkan view di dalam sebuah layout file
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        // ubah text dalam widget
         views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
         views.setTextViewText(R.id.appwidget_update, dateString);
         views.setTextViewText(R.id.appwidget_count, String.valueOf(count));
 
+        // membuat sebuah intent
         Intent intent = new Intent(context, NewAppWidget.class);
+        // mengirim sebuah broadcast ketika widget update
         intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
 
+        // membuat sebuah array kosong sebesar jumlah id widget yang dibuat
         int[] idArray = new int[] {appWidgetId};
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
 
+        // mengambil broadcast saat terdapat  broadcast
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         views.setOnClickPendingIntent(R.id.button_update, pendingIntent);
 
+        // memasuki edit mode untuk mengubah nilai pada sharedprefs
         SharedPreferences.Editor prefEditor = prefs.edit();
         prefEditor.putInt(COUNT_KEY+appWidgetId, count);
         prefEditor.apply();
